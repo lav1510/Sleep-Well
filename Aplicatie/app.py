f4dc643dcd8e7f2a1a41501a0c1d9bb98907641d
lav1510 <maria.bugarin@student.upt.ro>
2024-04-08 21:21:50 +0300

    Adaugare id-uri continut pentru design css

--- Aplicatie/app.py
+++ Aplicatie/app.py
@@ -10,29 +10,75 @@ data = (
 )
 
 app = Dash(__name__)
+app.title = "Sleep Well!"
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Sleep Well"),
-        html.P(
-            children=(
-                "O aplicatie pentru monitorizarea somnului."
-            ),
+        html.Div(
+            children=[
+                html.P(children="💤", className="header-emoji"),
+                html.H1(
+                    children="Sleep Well", className="titlu-header"
+                ),
+                html.P(
+                    children=(
+                        "Afla cat și cum dormi ca sa dormi mai mult si mai bine."
+                    ),
+                    className="descriere-header",
+                ),
+            ],
+            className="header",
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Bedtime"],
-                        "y": data["Sleep duration"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Somnul femeilor"},
-            },
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["Bedtime"],
+                                    "y": data["Sleep duration"],
+                                    "type": "lines",
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Ore de somn dormite",
+                                    "x": 0.05,
+                                },
+                                "colorway": ["#004C00"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["Bedtime"],
+                                    "y": data["Sleep efficiency"],
+                                    "type": "lines",
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Calitatea Somnului",
+                                    "x": 0.05,
+                                },
+                                "colorway": ["#0000b2"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
         ),
     ]
 )
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
