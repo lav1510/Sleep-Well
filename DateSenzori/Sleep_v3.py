042a5da8e5c56e9c55fe382838e207d5ca72627d
lav1510 <maria.bugarin@student.upt.ro>
2024-05-01 19:30:06 +0300

    used all modules classes

--- DateSenzori/Sleep_v3.py
+++ DateSenzori/Sleep_v3.py
@@ -1,4 +1,4 @@
-from gpiozero import DigitalInputDevice
+from gpiozero import DigitalInputDevice, LED, Button
 import threading
 import adafruit_dht
 import board
@@ -10,10 +10,16 @@ from monitor_miscare_radar import MonitorMiscareRadar
 from monitor_vibratii import MonitorVibratii
 from monitor_sunet import MonitorSunet
 
+import utilitare as ut
+
+#constane
+TIMP_VIBRATII_INCEPUT = 300 #in medie, un om se pune in pat cam in 5 minute
 
 #definirea moulelor cu senzori
+led = LED(13)
+buton = Button(26)
 modul_microfon = DigitalInputDevice(23)
-modul_miscare    = DigitalInputDevice(24)
+modul_miscare    = DigitalInputDevice(21)
 modul_miscare_ir = DigitalInputDevice(25)
 modul_vibratii = DigitalInputDevice(12)
 modul_lumina = DigitalInputDevice(27)
@@ -28,8 +34,11 @@ def citeste_miscare_pir():
               
 
 def inchide_module():
-        print("Iesire din program")
+        print("Iesire din program.")
+        led.close()
+        buton.close()
         modul_microfon.close()
+        modul_miscare.close()
         modul_miscare_ir.close()
         modul_vibratii.close()
         modul_lumina.close()
@@ -51,24 +60,42 @@ monitor_sunet = MonitorSunet(stare_sunet, modul_microfon)
 monitor_temp = MonitorTemperaturaUmiditate(stare_temp, modul_temperatura_umiditate)
 monitor_vibratii = MonitorVibratii(grad_vibratie, modul_vibratii)
 
-#pornirea firelor de executie
+
+#pornirea programului
 start = time.perf_counter()
 
-# thread_lumina = threading.Thread(target=monitor_lumina.monitorizeaza_lumina)
-# thread_lumina.start()
+ut.mod_consum_energie(buton, led)
+modul_vibratii.wait_for_active(TIMP_VIBRATII_INCEPUT)
 
-# thread_miscare = threading.Thread(target=monitor_miscare_radar.monitorizeaza_miscare)
-# thread_miscare.start()
+#pornirea firelor de executie
+thread_lumina = threading.Thread(target=monitor_lumina.monitorizeaza_lumina)
+thread_lumina.start()
 
+thread_miscare_radar = threading.Thread(target=monitor_miscare_radar.monitorizeaza_miscare)
+thread_miscare_radar.start()
 
-# #setarea starilor
-# time.sleep(30)
-# stare_temp.set()
-# stare_lumina.set()
+thread_sunet = threading.Thread(target=monitor_sunet.monitorizeaza_sunet)
+thread_sunet.start()
 
-# #asteptare finalizare fire de executie
-# thread_dht.join()
-# # thread_lux.join()
+thread_temperatura_umiditate = threading.Thread(target=monitor_temp.monitorizeaza_temperatura_umiditate)
+thread_temperatura_umiditate.start()
+
+thread_vibratie = threading.Thread(target=monitor_vibratii.monitorizeaza_vibratie)
+thread_vibratie.start()
+
+#setarea starilor
+time.sleep(30)
+monitor_lumina.stare.set()
+monitor_miscare_radar.grad_miscare = 1 #doar pentru test
+monitor_sunet.stare.set()
+monitor_temp.stare.set()
+
+#asteptare finalizare fire de executie
+thread_lumina.join()
+thread_miscare_radar.join()
+thread_sunet.join()
+thread_temperatura_umiditate.join()
+thread_vibratie.join()
 
 # print(f'Medie temperatura finala {medie_temp_final}, medie umiditate finala {medie_umid_final}')
 # print (f'Timp lumina {secunde_luminozitate}.')
