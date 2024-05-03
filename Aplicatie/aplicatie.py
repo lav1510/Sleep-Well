37dcb8df08aaf9825ed57f63b5045f0c7987b68f
lav1510 <maria.bugarin@student.upt.ro>
2024-05-03 14:29:44 +0300

    testing db connection

--- Aplicatie/aplicatie.py
+++ Aplicatie/aplicatie.py
@@ -2,10 +2,20 @@
 import pandas as pd
 from dash import Dash, Input, Output, dcc, html
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 import configurare_mongo_privat
 
-client = MongoClient(configurare_mongo_privat.CONNECTION_STRING)
+client = MongoClient(configurare_mongo_privat.CONNECTION_STRING, server_api=ServerApi('1'))
+
+#confirmare conexiune
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
 baza_date = client['Sleep']['SleepProject']
 data = pd.DataFrame(list(baza_date.find()))
 
