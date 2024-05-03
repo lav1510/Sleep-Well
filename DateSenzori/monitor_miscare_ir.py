d44be84c96e35f6dd1ae675037563ee00b296232
lav1510 <maria.bugarin@student.upt.ro>
2024-05-03 14:45:22 +0300

    modified pir class according to specifications

--- DateSenzori/monitor_miscare_ir.py
+++ DateSenzori/monitor_miscare_ir.py
@@ -2,30 +2,25 @@ from gpiozero import DigitalInputDevice
 import time
 import threading
 
+#timpul maxim de asteptare dupa pir este de 5 minute
+TIMP_PIR = 30
 
 class MonitorMiscareIR:
-    def __init__(self, stare: int,  modul_miscare_ir: DigitalInputDevice):
-        if not isinstance(stare, int):
-            raise TypeError("Parametrul 'stare' trebuie sa fie obiect de tipul int!")
-
+    def __init__(self, modul_miscare_ir: DigitalInputDevice):
         if not isinstance(modul_miscare_ir, DigitalInputDevice):
             raise TypeError("Parametrul 'modul_miscare_ir' trebuie sa fie obiect de tipul DigitalInputDevice!")
 
         self.modul_miscare_ir = modul_miscare_ir
-        self.stare = stare
         self.modul_miscare_ir_activat = 0
 
 
     def monitorizeaza_miscare_ir(self):
         print("Monitorizare miscare prin PIR.")
 
-        #relevant doar cand starea actuala este 1 (treaz in pat)
-        while self.stare == 1 and not self.modul_miscare_ir_activat:        
-            if self.modul_miscare_ir.value :
-                self.modul_miscare_ir_activat = 1
-                print("PIR activ.")
-                break
-            time.sleep(1)
+        self.modul_miscare_ir.wait_for_active(TIMP_PIR)
+        if self.modul_miscare_ir.value :
+            self.modul_miscare_ir_activat = 1
+            print("PIR activ.")
 
         print("Monitorzare PIR incheiata.")
 
@@ -33,14 +28,9 @@ class MonitorMiscareIR:
 if __name__ == "__main__":
     start = time.perf_counter()
     modul_miscare_ir = DigitalInputDevice(25)
-    stare_miscare_ir = 1
-    monitor_miscare_ir = MonitorMiscareIR(stare_miscare_ir, modul_miscare_ir)
+    monitor_miscare_ir = MonitorMiscareIR(modul_miscare_ir)
 
-    thread_pir = threading.Thread(target=monitor_miscare_ir.monitorizeaza_miscare_ir)
-    thread_pir.start()
-    # time.sleep(10)
-    # monitor_miscare_ir.stare = 2
-    thread_pir.join()
+    monitor_miscare_ir.monitorizeaza_miscare_ir()
 
     print("Iesire din program")
     modul_miscare_ir.close()
